abd2a6fff7f5acefabbc4e62ac5a7fa31a7c8127
StopselLocke <git@hilbo.de>
2018-10-05 11:33:32 +0000

    install Vundle via git clone

--- setup.sh
+++ setup.sh
@@ -34,6 +34,12 @@ then
 fi
 
 linkFile ".vim"
+# vim vundle
+if [ ! -d ~/.vim/bundle/Vundle.vim ]
+then
+  $(git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim)
+fi
+
 linkFile ".vimrc"
 linkFile ".gitconfig"
 linkFile ".tmux.conf"
