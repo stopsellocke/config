2e09725adcee5ea4e9732976b81551698db08138
StopselLocke <git@hilbo.de>
2018-11-15 23:10:12 +0100

    added tmux plugin manager

--- setup.sh
+++ setup.sh
@@ -37,6 +37,11 @@ fi
 
 linkFile ".gitconfig"
 linkFile ".tmux.conf"
+if [ ! -d ~/.tmux/plugins/tpm ]
+then
+  $(git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm)
+  $(~/.tmux/plugins/tpm/bin/install_plugins)
+fi
 
 if [ ! -d ~/.ssh ]
 then
